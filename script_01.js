a45c54f3f9d54bedc6a86fc6ba8cbff773234210
Peter Weber <peter-aalen@gmx.net>
2023-04-06 10:27:04 +0200

    Vorbereitung Array

--- script_01.js
+++ script_01.js
@@ -7,7 +7,20 @@
  * 
  */
 
+/*** 02. Theorie: Array */
+let arr;
+// arr = new Array(); // Konstruktor
+arr = []; // Literal
+arr = [2,7,11,4];
+arr = [true,false,true];
+arr = ["Wizzy","is","cool"];
 
+
+output(arr);
+output(arr.length); // Anzahl der Elemente
+output(arr[0]); // Index 0(1. Position im Arry)
+output(arr[3]); // letzte Position konkret
+output(arr[arr.length - 1]); // letzte Position / allgemein
 
 
 /**
@@ -20,15 +33,17 @@
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
+output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
 
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
-                PUNCT;
+                word5 + GAP +
+                word6 + PUNCT;
 
     return str;
 }
